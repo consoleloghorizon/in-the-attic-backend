524e385a0b7a38d9cad8a225bdc8ca0106c6b366
Kevin <abhxi@bitbucket.org>
2019-01-26 16:54:11 -0500

    Not disabling buttons correctly

--- player_screen/src/components/base/Game.js
+++ player_screen/src/components/base/Game.js
@@ -1,5 +1,28 @@
 import React from "react";
 import GameComponents from "../ingame";
+import { promises } from "fs";
+
+const ChoiceState = {
+    type: "ChoicePhase",
+    prompt: {
+        main: "Bathroom",
+        sub: "Shower",
+    },
+    choices: {
+        list: ["plunger", "toilet", "shower head", "shower curtain", "toaster", "rubber ducky"],
+        votes: 1,
+    },
+}
+
+const VotePhaseState = {
+    ...ChoiceState,
+    type: "VotePhase",
+    choices: {
+        list: ["plunger", "toilet", "shower head", "shower curtain", "rubber ducky"],
+        votes: 2,
+    }
+}
+
 /**
  * Father Component of all of the game states. Connect socket to game and change this.state with information received from the server.
  * @property Socket Info to be called on constructor, connect this object with server game 
@@ -18,20 +41,32 @@ export class Game extends React.Component {
     socketInput(input){
         console.log(input);
     }
+    FakeSocket(input){
+        return new Promise((res, rej) => {
+            setTimeout(() => {
+                console.log(input);
+                res({message: "successfull"})
+            }, 1000);
+        });
+    }
 
     submitToSocket(input){
-        setTimeout(() => {
-            this.socketInput(input);
-            this.setState({ game: 1 });
-        }, 1000);
+        this.FakeSocket(input).then(answer => {
+            this.setState({game: 1});
+        })
     }
+
+    // submitAnswer(input){
+    //     FakeSocket(input)
+    //         .then(res => )
+    // }
 
     getGameComponent(){
         switch(this.state.game){
             case 0:
                 return <GameComponents.Answer submitFunc={(str) => this.submitToSocket(str)}/>;
             case 1:
-                return <GameComponents.Selection />;
+                return <GameComponents.Selection submitFunc={(str) => this.submitToSocket(str)} type={VotePhaseState.type} choices={VotePhaseState.choices.list}/>;
             default:
                 return <GameComponents.MainLobby />;
         }
