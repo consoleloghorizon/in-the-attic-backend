67917eda8fefdd51cfeece942da9efd0233f9c05
Charles Garnette <cgarnette@synergybis.com>
2019-01-27 12:32:25 -0500

    removed commented lines

--- server/src/app.js
+++ server/src/app.js
@@ -57,12 +57,6 @@ io.on('connection', (client) => {
         gameDriver.getRoom(data.gameCode).startGame();
         io.sockets.emit('start game', {status: true});
             
-        // // Added for debugging purposes, TAKE OUT
-        // // If not game will start on VIP's startgame function
-        // gameDriver.getRoom(data.gameCode).startPhase();
-        // const phaseInfo = gameDriver.getRoom(data.gameCode).getPhaseInfo();
-        // gameDriver.getRoom(data.gameCode).startAcceptingAnswers();
-        // io.sockets.in(data.gameCode).emit('start phase', { phaseInfo: phaseInfo });
     });
 
     client.on('add host', data => {
@@ -105,12 +99,6 @@ io.on('connection', (client) => {
             const player = gameDriver.getPlayerList()[client.id];
             io.sockets.to(host).emit('submission success', {isTrue: true, player});
 
-            // // Added for debugging purposes, TAKE OUT
-            // gameDriver.getRoom(data.gameCode).resolvePhase();
-            // gameDriver.getRoom(data.gameCode).startPhase();
-            // const phaseInfo = gameDriver.getRoom(data.gameCode).getPhaseInfo();
-            // gameDriver.getRoom(data.gameCode).startAcceptingAnswers();
-            // io.sockets.in(data.gameCode).emit('start phase', { phaseInfo: phaseInfo });
         } catch (error) {
             client.emit('submission success', { isTrue: false, error: "Something went wrong, submit again" });
         }
